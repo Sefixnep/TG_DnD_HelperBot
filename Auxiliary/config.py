acc9de505c1777289833ef929df197b32fe04314
Sefixnep <116432693+Sefixnep@users.noreply.github.com>
2024-08-17 17:48:57 +0700

    Update config.py

--- Auxiliary/config.py
+++ Auxiliary/config.py
@@ -1,6 +1,6 @@
 # command in console for konvert: auto-py-to-exe
 
 
-Bot = "TG_DnD_bot"
-BOT_TOKEN = "7437034068:AAEYndTl0Tk4gup25Wu08IWGoMVIMvGSQ_0"
-api_gpt = "sk-BNaS89ujVeNzWD1HrEUlGKi5-GSFSaEs0K7PKdGhqWT3BlbkFJXdGrX3F0IrysnEM43StCbmX7kMz3hIsE1Wbfedo4MA"
+Bot = ""
+BOT_TOKEN = ""
+api_gpt = ""
